289d048bc1d9bd251c21740630c75667e5e4ed57
rojasdev <57605764+rojasdev@users.noreply.github.com>
2023-10-05 12:39:00 +0800

    Update index.php

--- index.php
+++ index.php
@@ -6,25 +6,25 @@
     <h3>Usage</h3>
     <ul>
         <li>Read all products
-            <ul><li>http://rdapi.herokuapp.com/product/read.php</li></ul>
+            <ul><li>http://your_ip_or_domain/product/read.php</li></ul>
         </li>
         <li>Read one product information
-            <ul><li>http://rdapi.herokuapp.com/product/read_one.php</li></ul>
+            <ul><li>http://your_ip_or_domain/product/read_one.php</li></ul>
         </li>
         <li>Create product
-            <ul><li>http://rdapi.herokuapp.com/product/create.php</li></ul>
+            <ul><li>http://your_ip_or_domain/product/create.php</li></ul>
         </li>
         <li>Display categories
-            <ul><li>http://rdapi.herokuapp.com/category/read.php</li></ul>
+            <ul><li>http://your_ip_or_domain/category/read.php</li></ul>
         </li>
         <li>Search for products
-            <ul><li>http://rdapi.herokuapp.com/product/search.php</li></ul>
+            <ul><li>http://your_ip_or_domain/product/search.php</li></ul>
         </li>
         <li>Update one product
-            <ul><li>http://rdapi.herokuapp.com/product/update.php</li></ul>
+            <ul><li>http://your_ip_or_domain/product/update.php</li></ul>
         </li>
         <li>Delete one record
-            <ul><li>http://rdapi.herokuapp.com/product/delete.php</li></ul>
+            <ul><li>http://your_ip_or_domain/product/delete.php</li></ul>
         </li>
     </ul>
 </body>
